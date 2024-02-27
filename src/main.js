36df08149a198fc9990bd6d7947c4680a7b2a0b6
ezdev2 <fansoaezra@gmail.com>
2024-02-27 10:28:09 +0300

    add tosttification

--- src/main.js
+++ src/main.js
@@ -4,5 +4,7 @@ import './style.css';
 import App from './App.vue';
 import router from './router';
 import i18n from './i18n';
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
-createApp(App).use(router).use(i18n).mount('#app');
+createApp(App).use(router).use(i18n).use(Toast).mount('#app');
